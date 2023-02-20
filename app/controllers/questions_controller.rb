3674902f07e87acdb98e9471f1bd9dd37d9ddc19
fakhriyya rahimova <rahimovafakhriyya@gmail.com>
2023-02-20 14:30:56 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ActionController::Base
+  def ask
+  end
+
+  def answer
+    @question = params[:ask]
+    @answer = if @question == 'I am going to work'
+                'great!'
+              elsif @question.include? '?'
+                'Silly question, get dressed and go to work!.'
+              else
+                'I don t care, get dressed and go to work!'
+              end
+  end
+end
